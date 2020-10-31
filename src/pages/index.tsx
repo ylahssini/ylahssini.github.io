2ab7244e0002e99089cf86033d5bcc6f3b65b41a
ylahssini <ylahssini@gmail.com>
2020-10-31 21:59:30 +0100

    remove blocked link tags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,23 +3,43 @@ import Layout from '@src/components/layout';
 import Side from '@src/components/side';
 import Content from '@src/components/content';
 
-const Home = () => (
-    <div>
-        <Head>
-            <title>Youssef Lahssini - Front End Developer</title>
-            <meta name="author" content="Youssef Lahssini" />
-            <link rel="icon" href="/favicon.ico" />
-            <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;400;700&display=swap" rel="stylesheet" />
-            <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css" />
-        </Head>
+const Home = () => {
+    return (
+        <div>
+            <Head>
+                <title>Youssef Lahssini - Front End Developer</title>
+                <meta name="author" content="Youssef Lahssini" />
+                <link rel="icon" href="/favicon.ico" />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;400;700&display=swap"
+                    rel="preload"
+                    as="style"
+                    onload="this.onload=null;this.rel='stylesheet'"
+                />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;400;700&display=swap"
+                    rel="stylesheet"
+                />
+                <link
+                    href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css"
+                    rel="preload"
+                    as="style"
+                    onload="this.onload=null;this.rel='stylesheet'"
+                />
+                <link
+                    href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css"
+                    rel="stylesheet"
+                />
+            </Head>
 
-        <Layout>
-            <div className="flex flex-row-reverse">
-                <Side />
-                <Content />
-            </div>
-        </Layout>
-    </div>
-)
+            <Layout>
+                <div className="flex flex-row-reverse">
+                    <Side />
+                    <Content />
+                </div>
+            </Layout>
+        </div>
+    );
+}
 
 export default Home;
