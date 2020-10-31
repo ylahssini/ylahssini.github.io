21409d98c2bd87b5149a0953ebf1963c46f94aae
ylahssini <ylahssini@gmail.com>
2020-10-31 17:11:46 +0100

    add images

--- next.config.js
+++ next.config.js
@@ -4,4 +4,11 @@ const debug = process.env.NODE_ENV !== "production";
 module.exports = withYaml({
     basePath: '',
     assetPrefix: '/',
+    images: {
+        deviceSizes: [150],
+        iconSizes: [],
+        domains: [],
+        path: '/images',
+        loader: 'default',
+    },
 });
