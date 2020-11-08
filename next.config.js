89116ae23fac976e4a9c0f36616d9b051d3b94a2
ylahssini <ylahssini@gmail.com>
2020-11-09 00:02:25 +0100

    fix purge css

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const withYaml = require('next-plugin-yaml');
+const withCSS = require('@zeit/next-css');
 
-module.exports = withYaml({
+module.exports = withCSS(withYaml({
     basePath: '',
     assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
-});
+}));
