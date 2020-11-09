f4de302aa74b73450f0f9fc71b35a1d4a6e7a595
ylahssini <ylahssini@gmail.com>
2020-11-09 23:32:05 +0100

    hope it works 😝

--- postcss.config.js
+++ postcss.config.js
@@ -1,25 +1,21 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-/* eslint-disable @typescript-eslint/ban-ts-comment */
-/* eslint-disable global-require */
-// @ts-ignore
-const purgecss = require('@fullhuman/postcss-purgecss')({
-    content: ['./src/**/*.jsx'],
-
-    defaultExtractor: (content) => {
-        const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
-        const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || [];
-
-        return broadMatches.concat(innerMatches);
-    },
-});
-
 module.exports = {
-    plugins: [
-        'tailwindcss',
-        'postcss-preset-env',
-        require('autoprefixer'),
-        ...process.env.NODE_ENV === 'production'
-            ? [purgecss]
-            : [],
-    ],
+    plugins: {
+        tailwindcss: {},
+        'postcss-preset-env': { stage: 2 },
+        ...(process.env.NODE_ENV === 'production'
+            ? {
+                '@fullhuman/postcss-purgecss': {
+                    content: ['./src/pages/**/*.{js,jsx,ts,tsx}', './src/components/**/*.{js,jsx,ts,tsx}'],
+                    defaultExtractor: (content) => {
+                        const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
+
+                        // Capture classes within other delimiters like .block(class="w-1/2") in Pug
+                        const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || []
+
+                        return broadMatches.concat(innerMatches);
+                    },
+                },
+            }
+            : {}),
+    },
 };
