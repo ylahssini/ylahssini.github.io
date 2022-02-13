343ee22a923291a7ed1655fa9cd961d823a6ee3a
ylahssini <ylahssini@gmail.com>
2022-02-13 14:32:30 +0100

    fix bug of swiper css caused in production mode

--- postcss.config.js
+++ postcss.config.js
@@ -3,20 +3,5 @@ module.exports = {
     plugins: {
         tailwindcss: {},
         'postcss-preset-env': { stage: 2 },
-        ...(process.env.NODE_ENV === 'production'
-            ? {
-                '@fullhuman/postcss-purgecss': {
-                    content: ['./src/pages/**/*.{js,jsx,ts,tsx}', './src/components/**/*.{js,jsx,ts,tsx}'],
-                    defaultExtractor: (content) => {
-                        const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
-
-                        // Capture classes within other delimiters like .block(class="w-1/2") in Pug
-                        const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || []
-
-                        return broadMatches.concat(innerMatches);
-                    },
-                },
-            }
-            : {}),
     },
 };
