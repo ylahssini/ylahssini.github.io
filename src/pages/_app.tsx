46d3c014be07bcbb7203c88960607620928ec955
ylahssini <ylahssini@gmail.com>
2024-06-04 23:13:39 +0100

    🔨 push last commit for this version

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,6 @@ import type { AppProps } from 'next/app';
 import { Cairo, Caveat } from 'next/font/google'
 import '@src/assets/styles/index.css';
 
-const GOOGLE_FONT_HREF = 'https://fonts.googleapis.com/css2?family=Cairo:wght@200;400;700&family=Caveat&display=swap';
-
 const cairo = Cairo({
     weight: ['200', '400', '700'],
     style: ['normal'],
