64cc42e6b1774737300d261b3806d673ec86b499
ylahssini <ylahssini@gmail.com>
2024-09-05 23:43:53 +0100

    🔧 config tailwind CSS

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,6 +5,40 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			fontFamily: {
+				body: ['"Public Sans"'] 
+			},
+
+			colors: {
+				'codgray': {
+					50: '#f6f7f6',
+					100: '#e3e4e3',
+					200: '#c6c9c6',
+					300: '#a2a7a1',
+					400: '#7e847d',
+					500: '#636963',
+					600: '#4e534e',
+					700: '#414540',
+					800: '#363936',
+					900: '#2f322f',
+					950: '#0e0f0e',
+				},
+
+				'trinidad': {
+					50: '#fff6ed',
+					100: '#feead6',
+					200: '#fcd1ac',
+					300: '#f9b078',
+					400: '#f68541',
+					500: '#f3631c',
+					600: '#e24912',
+					700: '#bd3611',
+					800: '#962c16',
+					900: '#792615',
+					950: '#411109',
+				},
+			},
+
 			boxShadow: {
 				'sun': '0 0 4px 1px rgba(255, 252, 238, 0.8)',
 				'moon': '0 0 2px 0 rgba(255, 255, 255, 0.5)',
