7b906fe0f0a030482382c6e3e6620d226c05fcc6
ylahssini <ylahssini@gmail.com>
2024-10-10 23:43:35 +0100

    🌐 config & initialize the i18n

---
+++ src/i18n/ui.ts
@@ -0,0 +1,32 @@
+export const defaultLang = 'en';
+
+export const ui: Record<string, any> = {
+    en: {
+        'menu.home': 'Home',
+        'menu.about': 'About me',
+        'menu.experiences': 'Experiences',
+        'menu.projects': 'Projects',
+        'menu.contact': 'Contact',
+
+        'home.hello': 'Hello!',
+        'home.my_name': 'My name is',
+        'home.see_about': 'See more about me',
+        'home.passionate': 'A passionate front-end',
+        'home.developer': 'Developer',
+        'home.scroll_down': 'Scroll down',
+    },
+    fr: {
+        'menu.home': 'Accueil',
+        'menu.about': 'À propos',
+        'menu.experiences': 'Expériences',
+        'menu.projects': 'Projets',
+        'menu.contact': 'Contact',
+
+        'home.hello': 'Bonjour!',
+        'home.my_name': 'Je m\'appelle',
+        'home.see_about': 'En savoir plus sur moi',
+        'home.passionate': 'un développeur Front-end',
+        'home.developer': 'passioné',
+        'home.scroll_down': 'Défiler vers le bas',
+    },
+};
